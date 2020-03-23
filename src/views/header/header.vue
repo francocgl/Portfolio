624335e41f40baad60851db86c4d821e48eadb13
Franco Cagliolo <34115278+francocgl@users.noreply.github.com>
2020-03-23 09:54:17 -0300

    add

---
+++ src/views/header/header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <header class="col-12 border">
+            <h1>FM</h1>
+            <nav>
+                <ul>
+                    <li><a>Home</a></li>
+                    <li><a>About</a></li>
+                    <li><a>Work</a></li>
+                    <li><a>Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
