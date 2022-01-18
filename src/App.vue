214a49a72b3a853b2b4f033830cdb1a0c8502849
franco <cagliolo@gmail.com>
2022-01-18 18:56:20 -0300

    UPLOAD

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <transition  name="fade" mode="out-in" >
         <router-view />
     </transition>  
-
   </div>
 </template>
     
@@ -20,48 +19,5 @@ export default {
   #app{
     overflow: hidden;
   }
-
-  .fade-enter-active {
-    transition: all .600ms cubic-bezier(1.0, 0.5, 0.8, 1.0);
-    animation: fade-in 1s ;
-  }
-  .fade-leave-active {
-    /* transition: all 1.4s cubic-bezier(1.0, 0.5, 0.8, 1.0); */
-    animation: fade-in 1s reverse;
-  }
- 
-    
-    @keyframes fade-in {
-        0% {
-            opacity: 0;
-            
-        }
-    
-        100% {
-            opacity: 1;
-        }
-    }
-
-    @keyframes fade-out {
-        0% {
-            transform: translateX(1000px);
-            
-        }
-    
-        100% {
-            transform: translateX(0px);
-            opacity: 1;
-        }
-    }
-  
-
-
-  .overlay{
-    height: 100%;
-    width: 0;
-    position: absolute;
-    background:var(--darkblue-color)
-  }
-
   
 </style>
